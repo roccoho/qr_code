638426881d5949831fc9ebbc033a65a7cf7f6ea2
rocco <103323204+roccoho@users.noreply.github.com>
2023-01-02 19:27:05 +0800

    update

--- qr_code.py
+++ qr_code.py
@@ -68,5 +68,5 @@ def result():
             return redirect(url_for("bridge", error=error))
 
 
-if __name__ == "__main__": 
-    app.run(debug=True)
+# if __name__ == "__main__": 
+#     app.run(debug=True)
